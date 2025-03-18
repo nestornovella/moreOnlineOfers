8411a57e31d2d04a760938fbc3c307828b99901e
Nestor novella <importarnorth@gmail.com>
2025-03-18 16:32:21 -0300

    changes

--- app/src/app/carrito/carritoHook.tsx
+++ app/src/app/carrito/carritoHook.tsx
@@ -1,13 +1,9 @@
-import {  useState } from 'react';
-import { useCartStore } from '../store/cartStore';
+
 
 
 function useCarrito() {
 
-    const { cart } = useCartStore()
-    const [cartProps, setCartProps] = useState({
-        total: 0
-    }) 
+   
 
    
 
