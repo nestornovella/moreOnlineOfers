884c8c68586324212af46f7584c85330e656be9b
Nestor novella <importarnorth@gmail.com>
2025-03-19 11:13:07 -0300

    changes

--- app/src/app/api/categories/id/[id]/route.tsx
+++ app/src/app/api/categories/id/[id]/route.tsx
@@ -1,16 +1,24 @@
 import { prismaClient } from "@/helpers/singeltonPrisma/prismaClient";
 import { NextResponse, NextRequest } from "next/server";
 
-
-export async function GET(request: NextRequest, { params }: {params:{id:string}}) {
+// GET: Obtener una categoría por ID
+export async function GET(
+  request: NextRequest,
+  context: { params: { id: string } }
+) {
   try {
-    const { id } = params;
+    const { id } = context.params;
     const category = await prismaClient.category.findUnique({
       where: { id },
       include: {
         subCategory: true,
       },
     });
+
+    if (!category) {
+      return NextResponse.json({ error: "Categoría no encontrada" }, { status: 404 });
+    }
+
     return NextResponse.json(category, { status: 200 });
   } catch (error) {
     if (error instanceof Error) {
@@ -19,11 +27,15 @@ export async function GET(request: NextRequest, { params }: {params:{id:string}}
   }
 }
 
-export async function PUT(request: NextRequest, { params }: {params:{id:string}}) {
+// PUT: Actualizar una categoría por ID
+export async function PUT(
+  request: NextRequest,
+  context: { params: { id: string } }
+) {
   try {
-    const { id } =  params;
+    const { id } = context.params;
     const body = await request.json();
-    
+
     const updated = await prismaClient.category.update({
       where: { id },
       data: body,
@@ -38,21 +50,22 @@ export async function PUT(request: NextRequest, { params }: {params:{id:string}}
   }
 }
 
-export async function DELETE(request: NextRequest,{ params }: {params:{id:string}}) {
+// DELETE: Eliminar una categoría por ID
+export async function DELETE(
+  request: NextRequest,
+  context: { params: { id: string } }
+) {
   try {
-    
-    const {id} = await params;
-    const categoryExist = await prismaClient.category.findUnique({where:{id}})
-    if (!categoryExist) throw new Error('la categoria no existe')
+    const { id } = context.params;
 
-    const deletedCategory = await prismaClient.category.delete({where: {id}});
-      
-    return NextResponse.json(deletedCategory, {status: 200});
+    const deletedCategory = await prismaClient.category.delete({
+      where: { id },
+    });
+
+    return NextResponse.json(deletedCategory, { status: 200 });
   } catch (error) {
     if (error instanceof Error) {
       return NextResponse.json({ error: error.message }, { status: 500 });
     }
   }
 }
-
-
