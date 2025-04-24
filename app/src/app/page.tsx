c9734860a55214dda783c5a0a764e8990abaaa8c
Nestor novella <importarnorth@gmail.com>
2025-04-24 14:39:34 -0300

    changes

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -6,22 +6,20 @@ import Header from "./components/header/Header";
 import ProductsMainSection from "./components/ProductsMainSectio";
 import { useProductStore } from "./store/productsStore";
 import TransitionPage from "./components/containers/TransitionPage";
-import { useSearchParams } from "next/navigation";
+
 
 
 
 
 export default function Home() {
   
-  const searchParams = useSearchParams()
-  const sellerId = searchParams.get('seller')
-
+  
   const { initialCharge, products } = useProductStore()
 
   useEffect(() => {
 
     if (!products.length) {
-      initialCharge(sellerId)
+      initialCharge()
     }
     
   }, [])
