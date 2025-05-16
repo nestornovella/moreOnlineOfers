69f5c937c1f26e72d483b034f8519e3a2a21a97a
Nestor novella <importarnorth@gmail.com>
2025-05-16 09:12:22 -0300

    ges

--- app/src/app/components/render/ProductsRender.tsx
+++ app/src/app/components/render/ProductsRender.tsx
@@ -3,29 +3,29 @@ import { useProductStore } from "@/app/store/productsStore"
 import ProductCard from "../card/ProductCard"
 
 interface Props {
-    category: CategoryIF
+  category: CategoryIF
 }
 
-function ProductRender({ category }: Props) { 
-    const { products } = useProductStore()
+function ProductRender({ category }: Props) {
+  const { products } = useProductStore()
 
-    // Filtramos los productos por categoría
-    const filteredProducts = products.filter(pr => 
-        pr.categories.some(p => p.name.includes(category.name))
-    )
+  // Filtramos los productos por categoría
+  const filteredProducts = products.filter(pr =>
+    pr.categories.some(p => p.name.includes(category.name))
+  )
 
-    return (
-        <div className="grid grid-cols-2 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4  gap-2">
-            {filteredProducts.map((prod, index) => {
-                const isLastOdd = index % 2 === 0 && index === filteredProducts.length - 1
-                return (
-                    <div key={prod.id} className={isLastOdd ? "col-span-2" : ""}>
-                        <ProductCard product={prod} />
-                    </div>
-                )
-            })}
-        </div>
-    )
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-2 ">
+      {filteredProducts.map((prod, index) => {
+        const isLastOdd = index % 2 === 0 && index === filteredProducts.length - 1
+        return (
+          <div key={prod.id} className={`h-full ${isLastOdd ? "col-span-2" : ""}`}>
+            <ProductCard product={prod} />
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default ProductRender
